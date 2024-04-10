ec47a75616a486a5a0e29f16284e884f19d80f59
Larissa Isabela <166644266+larissaisab07@users.noreply.github.com>
2024-04-10 16:36:32 -0300

    Create Disciplina.java

---
+++ Disciplina.java
@@ -0,0 +1,29 @@
+public class classeDisciplina {                             
+   public String nome;                                      
+   private String codigo;                                   
+                                                            
+   public classeDisciplina (String nome, String codigo) {   
+	   this.nome = nome;                                    
+	   this.codigo = codigo;                                
+	                                                        
+   }                                                        
+   public String getNome () {                               
+	   return nome;                                         
+	                                                        
+   }                                                        
+   public void setNome (String nome) {                      
+	   this.nome = nome;                                    
+	                                                        
+   }                                                        
+   private String getCodigo () {                            
+	   return codigo;                                       
+	                                                        
+   }                                                        
+   private void setCodigo (String codigo) {                 
+	   this.codigo = codigo;                                
+	                                                        
+   }                                                        
+                                                            
+                                                            
+}                                                           
+                                                            
