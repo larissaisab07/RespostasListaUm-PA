87f013eed9c2b0337be968a3f8574c25a2f86527
Larissa Isabela <166644266+larissaisab07@users.noreply.github.com>
2024-04-10 16:37:00 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,21 @@
+public class classeCalculadora {
+ public int a;
+ public int b;
+ 
+ public int somar (int a, int b) {
+	 return (a + b);
+	 
+ }
+ public int subtrair (int a, int b) {
+	 return (a - b);
+	 
+ }
+ public int multiplicar (int a, int b) {
+	 return (a * b);
+	 
+ }
+ public int dividir (int a, int b) {
+	 return (a / b);
+	 
+ }
+}
